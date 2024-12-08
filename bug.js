40d05350cfa5c967e1fb90f5a85c3ceb80bbc1f4
BugHunter-X <askzenai@gmail.com>
2024-12-08 10:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-md">
+  <!-- Content -->
+</div>
+```
+This code uses Tailwind's gradient utility classes. However, if the `to-purple-500` class is missing or mistyped, the gradient will not render properly. The error will likely not be directly apparent in the browser's developer console.
